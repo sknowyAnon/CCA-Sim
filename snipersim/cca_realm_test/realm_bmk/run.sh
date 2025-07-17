8da5497695fbe5e27d6629b2a7d30d72d5138ec3
sknowyAnon <206851734@qq.com>
2025-07-17 13:56:33 +0800

    Update run.sh

--- snipersim/cca_realm_test/realm_bmk/run.sh
+++ snipersim/cca_realm_test/realm_bmk/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./realm_bmk/realm1 & ./realm_bmk/realm2
+$SNIPER_HOME/dynamorio/bin64/drrun -c $SNIPER_HOME/dynamorio/clients/lib64/release/libdr-frontend.so -roi 0 -f 0 -d 0 -b 0 -o trace1 -e 0 -s 0 -r 0 -stop 0 -verbose -- ./realm_bmk/realm1 &
+$SNIPER_HOME/dynamorio/bin64/drrun -c $SNIPER_HOME/dynamorio/clients/lib64/release/libdr-frontend.so -roi 0 -f 0 -d 0 -b 0 -o trace2 -e 0 -s 0 -r 0 -stop 0 -verbose -- ./realm_bmk/realm2
 
 wait
 echo "[CCA] All Realm VMs finished"
